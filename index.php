e8d2bbc987b3b886ee05433e4f0f6b53df94d340
Hevelmo <hevelmo060683@gmail.com>
2020-09-12 12:00:16 -0500

    add index php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Nupali v2</title>
+    </head>
+    <body>
+        This Nupali v2
+    </body>
+</html>
